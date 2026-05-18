9f2baf924b90426c4968d3e6fb6b25fbc19ff9b7
jiwoo1012 <jiwoo6306@gmail.com>
2026-05-18 13:15:53 +0900

    우딘 카피  홈페이지

---
+++ js/index.js
@@ -0,0 +1,11 @@
+<script>
+    let gnb=document.querySelector(".gnb")
+        let nav=document.querySelector(".nav")
+
+        gnb.addEventListener("mouseenter", function(){
+            nav.classList.add("on")
+        })
+        gnb.addEventListener("mouseleave", function(){
+            nav.classList.remove("on")
+        })
+</script>
