2b94e141b2aac0629ec63c1014a3b88e67fbb9db
jiwoo1012 <jiwoo6306@gmail.com>
2026-05-19 17:54:29 +0900

    0519 수정본

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
-<script>
-    let gnb=document.querySelector(".gnb")
-        let nav=document.querySelector(".nav")
-
-        gnb.addEventListener("mouseenter", function(){
-            nav.classList.add("on")
-        })
-        gnb.addEventListener("mouseleave", function(){
-            nav.classList.remove("on")
-        })
-</script>
+let gnb=document.querySelector(".gnb")
+    let nav=document.querySelector(".nav")
+    gnb.addEventListener("mouseenter", function(){
+         nav.classList.add("on")
+    })
+    gnb.addEventListener("mouseleave", function(){
+        nav.classList.remove("on")
+    })
